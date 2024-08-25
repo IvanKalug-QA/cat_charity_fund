73e06219e5160b37d54f7aefc65127cf2a67b4c3
IVAN-GUY <kaluginivan2002@mail.ru>
2024-08-25 09:40:18 +0300

    Small fix

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings, EmailStr
 
 class Settings(BaseSettings):
     app_title: str = 'QRKot'
-    default: str = 'sqlite+aiosqlite:///./alembic.db'
+    default: str = 'sqlite+aiosqlite:///./qrkot.db'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
